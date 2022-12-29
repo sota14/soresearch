38d419d13af42c82989df80739f77dc9f8e33559
服部蒼汰 <sota@hattoriaonoMacBook-Pro.local>
2022-12-30 08:41:35 +0900

    SVG表示削除

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -14,11 +14,6 @@ const Layout: FC<LayoutProps> = ({ children }) => {
     <div>
       <header className="fixed t-0 h-12 flex justify-between px-2 border-b-2 border-white w-screen z-40 bg-def bg-none">
         <Link href="/" className="flex items-center ml-2">
-          <object
-            data="/logo.svg"
-            type="image/svg+xml"
-            className="xs:w-5 xs:h-5 w-7 h-7 m-2 ml-0 pointer-events-none"
-          ></object>
           <h1 className="header-site-title pt-1 xs:text-lg text-4xl avenir font-extrabold">
             SO RESEARCH
           </h1>
